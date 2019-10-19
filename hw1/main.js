78b616779397d2f384a6979e844628540620fbdd
Tanya Chudna <tanya.papushoi@gmail.com>
2019-10-19 17:03:43 +0300

    Fix code in homework1

--- hw1/main.js
+++ hw1/main.js
@@ -16,44 +16,38 @@ sumFromArray([1, 7, 3]);
 sumFromArray([1, undefined, 3, 5, -3]);
 sumFromArray([1, NaN, 3, 5, -3]);
 
-
 function maxFromArray(myArray) {
-    let max = null;
-
+    let maxNumber = Number.NEGATIVE_INFINITY;
     for (let number of myArray) {
-        if (max === null) {
-            max = number;
-        } else if (typeof number === 'number'&& number > max) {
-            max = number;
+
+        if (Number.isFinite(number) && number > maxNumber) {
+            maxNumber = number;
         }
     }
-    console.log(`Max number is ${max}`);
+    console.log(`Max number is ${maxNumber}`);
 }
 
 maxFromArray([1, -5, NaN, 9, -1, 'h', 0, undefined, '50', 7, -12, 46]);
-maxFromArray([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1]);
-maxFromArray([0, -1, -8, -2]);
+maxFromArray([3, 0, -5, 1, '44', -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1]);
+maxFromArray([undefined, -1, -8, -2]);
 maxFromArray([1, 7, 3]);
-maxFromArray([1, undefined, 3, 5, -3]);
+maxFromArray([NaN, 1, undefined, 3, 5, -3]);
 maxFromArray([1, NaN, 3, 5, -3]);
 
-
 function minFromArray(myArray) {
-    let min = null;
-
+    let minNumber = Number.POSITIVE_INFINITY;
     for (let number of myArray) {
-        if (min === null) {
-            min = number;
-        } else if (typeof number === 'number' && number < min) {
-            min = number;
+
+        if (Number.isFinite(number) && number < minNumber) {
+            minNumber = number;
         }
     }
-    console.log(`Min number is ${min}`);
+    console.log(`Min number is ${minNumber}`);
 }
 
 minFromArray([1, -5, NaN, 9, -1, 'h', 0, undefined, '50', 7, -12, 46]);
-minFromArray([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1]);
-minFromArray([-1, -8, -2]);
+minFromArray([NaN, 3, 0, -9, 1, 44, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1]);
+minFromArray([undefined, -1, -8, -2, -13]);
 minFromArray([1, 7, 3]);
 minFromArray([1, undefined, 3, 5, -3]);
-minFromArray([1, NaN, 3, 5, -3]);
+minFromArray([NaN, 1, NaN, 3, 5, -3]);
