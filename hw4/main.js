bd7cc280ba5e73c11277dba50af7e88dda513023
Tanya Chudna <tanya.papushoi@gmail.com>
2019-10-29 11:15:24 +0200

    Create homework 4

---
+++ hw4/main.js
@@ -0,0 +1,7 @@
+/*1.Closure - Create a constructor function which will use closure for working with private data. It should have 2 private methods and 2 private props which we can change only with that private methods.
+3.Создать новые методы для всех массивов:
+1) myForEach - тот же самый forEach
+2) myMap - тот же самый map
+3) mySort - тот же самый sort
+ */
+
