e948bdc7927866628f9022c6d694500fc238ce42
Tanya Chudna <tanya.papushoi@gmail.com>
2019-11-13 10:17:12 +0200

    Add factorial function

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -12,3 +12,19 @@ console.log(isPrime(0));                        // false
 console.log(isPrime(1));                        // false
 console.log(isPrime(17));                       // true
 console.log(isPrime(10000000000000));           // false
+
+//2. factorial - Returns a number that is the factorial of the given number.
+function factorial(num) {
+    for (let i = num; --i;) {
+        num *= i;
+        if (num === 0) {
+            return 1;
+        } else if (num < 0) {
+            return undefined;
+        }
+    }
+    return num;
+}
+console.log(factorial(0));                        // 1
+console.log(factorial(1));                        // 1
+console.log(factorial(6));                        // 720
